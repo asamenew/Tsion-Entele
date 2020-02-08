c626bd1e828a62805073bcd26093106f8071a57a
asamenew <60811870+asamenew@users.noreply.github.com>
2020-02-08 09:56:41 +0000

    Add files via upload

---
+++ www/microlinkplc/js/login.js
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<script type="text/javascript">
+function append()
+	{
+	text1=document.getElementById('newTxt').value;
+	text2=document.getElementById('oldTxt').innerHTML;
+	fullTxt=text2+' '+text1;
+	document.getElementById('oldTxt').innerHTML=fullTxt;
+	}
+</script>
+
+
+
